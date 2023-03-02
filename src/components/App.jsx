827926402f56365a32836eb3956e55535d56308e
Ramon Fávero <ramon.favero@hotmail.com>
2023-03-02 20:46:04 -0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import '../App.css';
+import Body from './Body';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+   
+    <div>
+       <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Icon } from '@iconify/react';
+import img from "../img/portfolio.png";
+
+
+
+function Header() {
+
+//     e.target.children[1].children[0].children[index].children[0].children[1].innerHTML = ""
+
+
+  
+    return (
+            <header id="header" className="header">
+            <div className="portfolio-image-name">
+            <img id="portfolio-pic" src={img} alt="selfie for portfolio" />
+            <div className="portfolio-name">
+            <span className="nome">Ramon Fávero</span>
+            <span className="subnome">soon to be front-end dev</span>
+            </div>
+              </div>
+              <div className="header-list-div">
+                <ul>
+              <li className="header-li header-options-text"><a href="#apresentation" className="headericon-row" ><Icon className="header-icon" icon="bx:home-alt" /><span>  Home</span></a></li>
+              <li className="header-li header-options-text"><a href="#aboutme" className="headericon-row"><Icon className="header-icon" icon="bx:id-card" /><span>   About Me</span></a></li>
+              <li className="header-li header-options-text"><a href="#projects" className="headericon-row" ><Icon className="header-icon" icon="bx:layer" /><span>  My Projects</span></a></li>
+              <li className="header-li header-options-text"><a href="#contacts" className="headericon-row" ><Icon className="header-icon" icon="bx:phone" /><span>  Contacts</span></a></li>
+              </ul>
+              </div>
+            </header>
+    )
+}
+export default Header; 

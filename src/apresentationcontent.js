8c57036bda65c8b27b05a41d82417c29a8d78f05
Ramon Fávero <ramon.favero@hotmail.com>
2023-03-14 19:28:19 -0300

    add new project photo

--- src/apresentationcontent.js
+++ src/apresentationcontent.js
@@ -1,8 +1,8 @@
 const apresentationcontent = [
 {id:1,
-title:"Certificados",
-img:"https://raw.githubusercontent.com/RamonFavero/ramonportfolio/main/src/img/udemy-certificate.jpg",
-corpo:"Certificado do Curso Full-Stack da Angela Yu:"
+title:"Keeper App",
+img:"../img/keeperapp.png",
+corpo:"Este foi um projeto que fiz me inspirando no google Keep, "
 },
 {id:2,
 title:"Certificados",
